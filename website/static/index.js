4d6feab0b66b60dc8243def2af25570f76be8c34
H Collingwood <hkcollingwood@gmail.com>
2023-10-07 12:13:55 +0100

    Added template for Earth page

--- website/static/index.js
+++ website/static/index.js
@@ -1,53 +1,207 @@
-import * as THREE from 'three';
+var rotationDelay = 3000
+var scaleFactor = 0.6
+var degPerSec = 6
+var angles = { x: -20, y: 40, z: 0}
+var colorWater = '#181236'
+var colorLand = '#F19BFE'
+var colorGraticule = '#181236'
+var colorCountry = '#F6C1BC'
 
-let earthRadius= 6378; //radius of sphere1(Earth)
 
-//setting up earth(globe)
-const mainSphere= new THREE.SphereGeometry(earthRadius,64,64); //creating geometry
-const textureimg= new THREE.TextureLoader().load('textures/earth.jpg');
-const texturebump= new THREE.TextureLoader().load('textures/bump.jpg');
-const texturespec= new THREE.TextureLoader().load('textures/spec.jpg')
-var material= new THREE.MeshPhongMaterial({ map:textureimg, bumpMap:texturebump, specularMap:texturespec }); //creating material& adding texture
-
-var earthmesh= new THREE.Mesh(mainSphere, material); //adding geometry and material to mesh
-
-// Setup renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('viz').appendChild(renderer.domElement);
-
-// Setup scene
-const scene = new THREE.Scene();
-scene.add(new THREE.AmbientLight(0xbbbbbb));
-scene.add(new THREE.DirectionalLight(0xffffff, 0.6));
-
-scene.add(earthmesh);
-
-//setting camera
-const camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,1,20000);
-camera.position.set(1425,8000,-6160); //This is for demo
-camera.lookAt(0,0,0);
-camera.updateProjectionMatrix();
-
-function animate(){
-  camera.updateProjectionMatrix();
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-
-  // Add camera controls
-    var controls= new OrbitControls(camera, renderer.domElement);
-    controls.target.set(0,0,0);
-    controls.minDistance=6385;
-    controls.maxDistance=40000;
-    controls.enableDamping=true;
-    controls.dampingFactor=0.05;
-    controls.mouseButtons = {
-        LEFT: THREE.MOUSE.ROTATE,
-        MIDDLE: THREE.MOUSE.DOLLY,
-        RIGHT: THREE.MOUSE.PAN
-    };
-
-//add controls.update() inside animate() function
+function enter(country) {
+  var country = countryList.find(function(c) {
+    return c.id === country.id
+  })
+  current.text(country && country.name || '')
 }
 
-animate();
+function leave(country) {
+  current.text('')
+}
+
+//
+// Variables
+//
+
+var current = d3.select('#current')
+var canvas = d3.select('#globe')
+var context = canvas.node().getContext('2d')
+var water = {type: 'Sphere'}
+var projection = d3.geoOrthographic().precision(0.1)
+var graticule = d3.geoGraticule10()
+var path = d3.geoPath(projection).context(context)
+var v0 // Mouse position in Cartesian coordinates at start of drag gesture.
+var r0 // Projection rotation as Euler angles at start.
+var q0 // Projection rotation as versor at start.
+var lastTime = d3.now()
+var degPerMs = degPerSec / 1000
+var width, height
+var land, countries
+var countryList
+var autorotate, now, diff, roation
+var currentCountry
+
+//
+// Functions
+//
+
+function setAngles() {
+  var rotation = projection.rotate()
+  rotation[0] = angles.y
+  rotation[1] = angles.x
+  rotation[2] = angles.z
+  projection.rotate(rotation)
+}
+
+function scale() {
+  width = document.documentElement.clientWidth
+  height = document.documentElement.clientHeight
+  canvas.attr('width', width).attr('height', height)
+  projection
+    .scale((scaleFactor * Math.min(width, height)) / 2)
+    .translate([width / 2, height / 2])
+  render()
+}
+
+function startRotation(delay) {
+  autorotate.restart(rotate, delay || 0)
+}
+
+function stopRotation() {
+  autorotate.stop()
+}
+
+function dragstarted() {
+  v0 = versor.cartesian(projection.invert(d3.mouse(this)))
+  r0 = projection.rotate()
+  q0 = versor(r0)
+  stopRotation()
+}
+
+function dragged() {
+  var v1 = versor.cartesian(projection.rotate(r0).invert(d3.mouse(this)))
+  var q1 = versor.multiply(q0, versor.delta(v0, v1))
+  var r1 = versor.rotation(q1)
+  projection.rotate(r1)
+  render()
+}
+
+function dragended() {
+  startRotation(rotationDelay)
+}
+
+function render() {
+  context.clearRect(0, 0, width, height)
+  fill(water, colorWater)
+  stroke(graticule, colorGraticule)
+  fill(land, colorLand)
+  if (currentCountry) {
+    fill(currentCountry, colorCountry)
+  }
+}
+
+function fill(obj, color) {
+  context.beginPath()
+  path(obj)
+  context.fillStyle = color
+  context.fill()
+}
+
+function stroke(obj, color) {
+  context.beginPath()
+  path(obj)
+  context.strokeStyle = color
+  context.stroke()
+}
+
+function rotate(elapsed) {
+  now = d3.now()
+  diff = now - lastTime
+  if (diff < elapsed) {
+    rotation = projection.rotate()
+    rotation[0] += diff * degPerMs
+    projection.rotate(rotation)
+    render()
+  }
+  lastTime = now
+}
+
+function loadData(cb) {
+  d3.json('https://unpkg.com/world-atlas@1/world/110m.json', function(error, world) {
+    if (error) throw error
+    d3.tsv('https://gist.githubusercontent.com/mbostock/4090846/raw/07e73f3c2d21558489604a0bc434b3a5cf41a867/world-country-names.tsv', function(error, countries) {
+      if (error) throw error
+      cb(world, countries)
+    })
+  })
+}
+
+// https://github.com/d3/d3-polygon
+function polygonContains(polygon, point) {
+  var n = polygon.length
+  var p = polygon[n - 1]
+  var x = point[0], y = point[1]
+  var x0 = p[0], y0 = p[1]
+  var x1, y1
+  var inside = false
+  for (var i = 0; i < n; ++i) {
+    p = polygon[i], x1 = p[0], y1 = p[1]
+    if (((y1 > y) !== (y0 > y)) && (x < (x0 - x1) * (y - y1) / (y0 - y1) + x1)) inside = !inside
+    x0 = x1, y0 = y1
+  }
+  return inside
+}
+
+function mousemove() {
+  var c = getCountry(this)
+  if (!c) {
+    if (currentCountry) {
+      leave(currentCountry)
+      currentCountry = undefined
+      render()
+    }
+    return
+  }
+  if (c === currentCountry) {
+    return
+  }
+  currentCountry = c
+  render()
+  enter(c)
+}
+
+function getCountry(event) {
+  var pos = projection.invert(d3.mouse(event))
+  return countries.features.find(function(f) {
+    return f.geometry.coordinates.find(function(c1) {
+      return polygonContains(c1, pos) || c1.find(function(c2) {
+        return polygonContains(c2, pos)
+      })
+    })
+  })
+}
+
+
+//
+// Initialization
+//
+
+setAngles()
+
+canvas
+  .call(d3.drag()
+    .on('start', dragstarted)
+    .on('drag', dragged)
+    .on('end', dragended)
+   )
+  .on('mousemove', mousemove)
+
+loadData(function(world, cList, airports) {
+  land = topojson.feature(world, world.objects.land)
+  countries = topojson.feature(world, world.objects.countries)
+  countryList = cList
+  
+  window.addEventListener('resize', scale)
+  scale()
+  autorotate = d3.timer(rotate)
+})
