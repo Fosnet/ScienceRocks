c8ccdba6ed8dd7eb1cef48d5d09878e436106c04
H Collingwood <hkcollingwood@gmail.com>
2023-10-07 17:58:25 +0100

    Pass and plot data points

--- website/views.py
+++ website/views.py
@@ -1,4 +1,8 @@
+from datetime import date
+
 from flask import Blueprint, render_template
+
+from utilities import DataType, get_data
 
 views = Blueprint('views', __name__)
 
@@ -7,8 +11,17 @@ def home():
     return render_template("home.html")
 
 @views.route('/earth')
-def earth(): 
-    return render_template("earth.html")
+def earth():
+    start_date = date(year=2000, month=1, day=1)
+
+    end_date = date.today()
+    data_type = DataType.FIREBALL
+
+    df = get_data(start_date, end_date, data_type)
+
+    data_json = df.to_json(orient='records')
+
+    return render_template("earth.html", data=data_json, headings=df.columns.values)
 
 @views.route('/mars')
 def mars():
